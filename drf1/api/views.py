6e22b4323d3183a74aeca6caf3dafe1515534b7c
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-08-19 19:45:57 +0530

    basic serializer and serialization

--- drf1/api/views.py
+++ drf1/api/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
@@ -14,6 +14,18 @@ def student_detail(request, pk):
     json_data = JSONRenderer().render(serializer.data)
     # print(json_data)
     return HttpResponse(json_data, content_type = 'application/json')
+
+#model object - single student data--can be done using jsonresponse too
+def student_detail(request, pk):
+    stu = Student.objects.get(id=pk)
+    # print(stu)
+    serializer = StudentSerializer(stu)
+    # print(serializer)
+    # print(serializer.data)
+    # json_data = JSONRenderer().render(serializer.data)
+    # print(json_data)
+    # return HttpResponse(json_data, content_type = 'application/json')
+    return JsonResponse(serializer.data, safe=True)
 
 #entire query set//all data
 def student_list(request):
